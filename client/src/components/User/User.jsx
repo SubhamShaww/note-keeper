78b4ae91733c3ea5e1879b3fbc0edbe789fa7536
SubhamShaww <sub.show@gmail.com>
2020-10-30 00:20:55 +0530

    h

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -4,7 +4,7 @@ import Notes from "./notes/Notes";
 import {connect} from "react-redux";
 
 function User(props) {
-    
+
     useEffect(() => {
         if(props.user) {
             return (
@@ -17,7 +17,7 @@ function User(props) {
             // when user is not logged in
             props.history.push('/');
         }
-    }); 
+    },[]); 
     
 }
 
