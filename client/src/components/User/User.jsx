d12a5a380aca32f0b26e656999d2b4d837f9e946
SubhamShaww <sub.show@gmail.com>
2020-10-31 00:10:01 +0530

    fdg

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -5,24 +5,25 @@ import {connect} from "react-redux";
 
 function User(props) { 
     
-    const renderContent = () => {
-        switch(props.user){
-          case null: // when not connected to server
-            return ;
-          case false: // when user is not logged in
-            props.history.push('/');
-          
-          default: // when user is logged in
-            return (
-              <React.Fragment>
-                <CreateArea />
-                <Notes />
-              </React.Fragment>
-            )
-        }
+  const renderContent = () => {
+      switch(props.user){
+        case null: // when not connected to server
+          return ;
+        case false: // when user is not logged in
+          props.history.push('/');
+        
+        default: // when user is logged in
+          return (
+            <React.Fragment>
+              <CreateArea />
+              <Notes />
+            </React.Fragment>
+          )
       }
+  }
 
-    return <div>{renderContent()}</div>
+  return <div>{renderContent()}</div>
+  
 }
 
 const mapStateToProps = (state) => {
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import SignIn from "./SignInCard";
+import {connect} from "react-redux";
 
 function Login() {
-    return (
-        <div className="card text-center my-5 mx-auto container">
-            <SignIn text="Sign In with " btnColor="btn-outline-danger" sym="fab fa-google" auth="/auth/google"/>
-            <SignIn text="Sign In with " btnColor="btn-outline-primary" sym="fab fa-facebook" auth="/auth/facebook"/>
-        </div>
-    );
+
+    const renderContent = () => {
+        switch(props.user){
+          case null: // when not connected to server
+            return ;
+          case false: // when user is not logged in
+            props.history.push('/');
+          
+          default: // when user is logged in
+            return (
+                <div className="card text-center my-5 mx-auto container">
+                    <SignIn text="Sign In with " btnColor="btn-outline-danger" sym="fab fa-google" auth="/auth/google"/>
+                    <SignIn text="Sign In with " btnColor="btn-outline-primary" sym="fab fa-facebook" auth="/auth/facebook"/>
+                </div>
+            )
+        }
+    }
+  
+    return <div>{renderContent()}</div>
+
 }
 
-export default Login;
+const mapStateToProps = (state) => {
+    return {
+        user: state.auth
+    }
+}
+
+export default connect(mapStateToProps)(Login);
