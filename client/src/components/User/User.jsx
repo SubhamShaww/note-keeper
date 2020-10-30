334f55125e03c1196787c29224c10f916f9fb82d
SubhamShaww <sub.show@gmail.com>
2020-10-30 15:22:25 +0530

    d

--- client/src/components/User/User.jsx
+++ client/src/components/User/User.jsx
@@ -3,27 +3,21 @@ import CreateArea from "./noteInput/CreateArea";
 import Notes from "./notes/Notes";
 import {connect} from "react-redux";
 
-function User(props) { 
-
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-    useEffect(() => {
-        if(props.user) {
-            setIsLoggedIn(true);
-        }
-    },[isLoggedIn]);
+function User(props) {
 
     const getNotes = () => {
-        if(isLoggedIn) {
-            return (
-                <div>
-                    <CreateArea />
-                    <Notes />
-                </div>
-            );
-        } else {
-            return <div></div>;
-        }
+        useEffect(() => {
+            if(props.user) {
+                return (
+                    <div>
+                        <CreateArea />
+                        <Notes />
+                    </div>
+                );
+            } else {
+                return <div></div>;
+            } 
+        },[]);
     } 
     
     return <div>{getNotes}</div>
