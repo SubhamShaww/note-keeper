43c1549734aa48330977a2aa3c6db390e36b1daf
SubhamShaww <sub.show@gmail.com>
2020-10-31 00:16:07 +0530

    s

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -2,22 +2,22 @@ import React from "react";
 import SignIn from "./SignInCard";
 import {connect} from "react-redux";
 
-function Login() {
+function Login(props) {
 
     const renderContent = () => {
         switch(props.user){
           case null: // when not connected to server
             return ;
           case false: // when user is not logged in
-            props.history.push('/');
-          
-          default: // when user is logged in
             return (
                 <div className="card text-center my-5 mx-auto container">
                     <SignIn text="Sign In with " btnColor="btn-outline-danger" sym="fab fa-google" auth="/auth/google"/>
                     <SignIn text="Sign In with " btnColor="btn-outline-primary" sym="fab fa-facebook" auth="/auth/facebook"/>
                 </div>
             )
+          
+          default: // when user is logged in
+            props.history.push('/user');
         }
     }
   
